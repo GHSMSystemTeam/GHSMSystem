6233a88d4b7f58bdef5759a3f53e56934192f7af
PanmeansBread <haitranduc60@gmail.com>
2025-05-30 14:34:55 +0700

    Deleted Classes and class repos
    -Staffs and admin will be deleted and will now by distinguised by the role attribute

--- src/main/java/com/GHSMSystemBE/GHSMSystem/APIs/DoctorAPI.java
+++
@@ -1,4 +0,0 @@
-package com.GHSMSystemBE.GHSMSystem.APIs;
-
-public class DoctorAPI {
-}
--- src/main/java/com/GHSMSystemBE/GHSMSystem/Repos/staffRepo.java
+++
@@ -1,9 +0,0 @@
-package com.GHSMSystemBE.GHSMSystem.Repos;
-
-import com.GHSMSystemBE.GHSMSystem.Dto.Staff;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface staffRepo extends JpaRepository<Staff, String> {
-}
