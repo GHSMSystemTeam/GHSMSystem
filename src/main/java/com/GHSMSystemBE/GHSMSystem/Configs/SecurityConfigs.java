85c8b6bf32223ccb225113da23cdd59f263ddc4a
Thinh-Tran <74083436+Thinh-Tran-0902@users.noreply.github.com>
2025-06-16 23:43:50 +0700

    change back SecurityConfig

--- src/main/java/com/GHSMSystemBE/GHSMSystem/Configs/SecurityConfigs.java
+++ src/main/java/com/GHSMSystemBE/GHSMSystem/Configs/SecurityConfigs.java
@@ -13,7 +13,7 @@ public class SecurityConfigs {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        /*http
+        http
                 // Disable CSRF for H2 console and API endpoints
                 .csrf(csrf -> csrf.disable())
 
@@ -29,12 +29,12 @@ public class SecurityConfigs {
                         .requestMatchers("/api/**").permitAll()
                         // Optional: require authentication for other endpoints
                         .anyRequest().permitAll()
-                );*/
+                );
 
-        HttpSecurity httpSecurity = http
+        /*HttpSecurity httpSecurity = http
                 .authorizeHttpRequests((authorize) -> authorize
                         .anyRequest().authenticated()
-                );
+                );*/
 
         return http.build();
     }
