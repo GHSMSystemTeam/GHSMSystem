038d92ee766a03db42898fd8b79131271d2533a0
PanmeansBread <haitranduc60@gmail.com>
2025-07-02 23:31:05 +0700

    remove unecessary code

--- src/main/java/com/GHSMSystemBE/GHSMSystem/Misc/FireBaseCMS/FirebaseProperties.java
+++ src/main/java/com/GHSMSystemBE/GHSMSystem/Misc/FireBaseCMS/FirebaseProperties.java
@@ -1,24 +1,4 @@
-<<<<<<< Updated upstream
-//package com.GHSMSystemBE.GHSMSystem.Misc.FireBaseCMS;
-//
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.core.io.Resource;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//@ConfigurationProperties(prefix = "firebase")
-//public class FirebaseProperties {
-//    private Resource serviceAccount;
-//
-//    public Resource getServiceAccount() {
-//        return serviceAccount;
-//    }
-//
-//    public void setServiceAccount(Resource serviceAccount) {
-//        this.serviceAccount = serviceAccount;
-//    }
-//}
-=======
+
 package com.GHSMSystemBE.GHSMSystem.Misc.FireBaseCMS;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
